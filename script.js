85291b46e2f9d9fd5c773446cc0a220d1a1b920d
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2023-11-28 09:11:12 +0000

    add files

--- script.js
+++ script.js
@@ -8,3 +8,42 @@ function evenNumber(x){
 
 const even = evenNumber(10)
 console.log(even)
+
+console.clear();
+
+// gives multiples of number given
+
+function table(num) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} X ${i} = ${num * i}`);
+  }
+}
+console.log("table");
+table(2);
+
+// reverse tables
+
+function reverseTable(num) {
+  for (let i = 10; i >= 1; i--) {
+    console.log(`${num} X ${i} = ${num * i}`);
+  }
+}
+
+console.log("Reverse table");
+reverseTable(3);
+
+// skipping one field in a table
+
+function skippingOneTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    if (i === 3) {
+      continue;
+    }
+    console.log(`${num} X ${i} = ${num * i}`);
+  }
+}
+console.log("Skipping multiple of 4 with 3");
+
+skippingOneTable(2);
+
+
