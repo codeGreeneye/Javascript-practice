6eb9a3e4df05f10f46646c25e3963abb4901c1f4
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2023-11-28 09:22:48 +0000

    add files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// 9nov even number
+
 function evenNumber(x){
     if(x % 2 === 0){
         return true;
@@ -9,7 +11,7 @@ const even = evenNumber(10)
 console.log(even)
 
 
-// 14nov assignment
+// 14nov assignment tables
 
 function table(num) {
   for (let i = 1; i <= 10; i++) {
@@ -32,3 +34,51 @@ reverseTable(3);
 
 
 
+// months name 15nov
+
+function getMonthName(month) {
+    switch (month) {
+      case 1:
+        console.log("january");
+        break;
+      case 2:
+        console.log("february");
+        break;
+      case 3:
+        console.log("march");
+        break;
+      case 4:
+        console.log("april");
+        break;
+      case 5:
+        console.log("may");
+        break;
+      case 6:
+        console.log("june");
+        break;
+      case 7:
+        console.log("july");
+        break;
+      case 8:
+        console.log("august");
+        break;
+      case 9:
+        console.log("september");
+        break;
+      case 10:
+        console.log("october");
+        break;
+      case 11:
+        console.log("november");
+        break;
+      case 12:
+        console.log("december");
+        break;
+      default:
+        console.log("no input");
+    }
+  }
+  
+
+  getMonth(11);
+  
